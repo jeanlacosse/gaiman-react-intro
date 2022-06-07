7b18601cf26e449bd89d37bb80bfb7fa5d93654f
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-07 11:20:06 -0500

    notes bitch

--- src/App.js
+++ src/App.js
@@ -6,17 +6,56 @@ import Comment from './comment'
 // here is how to pull in an exported file
 import greet from './greet.js/greet';
 
+// grab use state prop from react
+import { useState } from 'react';
+
 // App is a component
 // components return HTML
-function App (){
-  return (
-    <div className="App"> 
-    <h1>Comments App</h1>
+function App() {
+  const [appName, setAppName] = useState('Comments App')
+  // appNameInput wil hold value from input field
+  const [appNameInput, setAppNameInput] = useState('')
 
-{/* here I can write the component as part of the html itself */}
-    <Comment />
-    <Comment />
-    <Comment />
+  const onSubmitAppName = (evt) => {
+    evt.preventDefault(); // don't reload the page
+    console.log('onSubmitAppName', appNameInput)
+
+    // need to set the app name to the input value
+    setAppName(appNameInput);
+
+    // this will reset the input value to nothing
+    setAppNameInput('')
+  }
+
+  return (
+    <div className="App">
+      <h1>{appName}</h1>
+
+      <p>
+        <h3>Go ahead, rename the app</h3>
+        <form onSubmit={onSubmitAppName} >
+          <input
+            type="text"
+            placeholder="new app name"
+            // this is taking the appNameInput and reseting its value which will be ''
+            value={appNameInput}
+            onChange={(evt) => {
+              // $(this).val
+              console.log(evt.target, evt.target.value); // vanilla version of $(this), 
+              // the actual html input per line written
+
+              // update appNameInput to be value of this input element
+              setAppNameInput(evt.target.value)
+            }}
+          />
+          <button>Change it!</button>
+        </form>
+      </p>
+
+      {/* here I can write the component as part of the html itself */}
+      <Comment />
+      <Comment />
+      <Comment />
     </div>
   )
 }
